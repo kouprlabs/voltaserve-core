dd8f9211934b16f238e8bc17aed00d53fa2ae539
Anass Bouassaba <ebouassaba@gmail.com>
2025-06-22 09:42:31 +0200

    fixup! chore: add more views

--- Sources/Views/File/Icon/FileCellMetrics.swift
+++ Sources/Views/File/Icon/FileCellMetrics.swift
@@ -11,8 +11,8 @@
 import Foundation
 
 public enum FileCellMetrics {
-    static let iconSize = CGSize(width: 80, height: 80)
-    static let frameSize = CGSize(width: 160, height: 160)
-    static let cellSize = CGSize(width: 160, height: 260)
-    static let badgeOffset = CGSize(width: 5, height: 5)
+    public static let iconSize = CGSize(width: 80, height: 80)
+    public static let frameSize = CGSize(width: 160, height: 160)
+    public static let cellSize = CGSize(width: 160, height: 260)
+    public static let badgeOffset = CGSize(width: 5, height: 5)
 }
